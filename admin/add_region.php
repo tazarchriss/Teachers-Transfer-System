fe9ff0f98b0fc967ec5dad3b9ca6bf6a66eb71fd
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-05 23:37:57 +0300

    Create add_region.php

---
+++ admin/add_region.php
@@ -0,0 +1,39 @@
+<!-- This is the page for viewing all general secretaries -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TTS | Add Station</title>
+  <!-- start of admin css section -->
+  <?php include 'include/admincss.php'; ?>
+  <!-- end of admin css section -->
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed bg-light">
+
+<!-- start of navbar section -->
+<?php include("include/navbar.php"); ?>
+<!--  end of navbar section -->
+
+<div class="content-wrapper">
+<section class="content">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+        <h5 class="text-left  text-lightblue mx-2 mt-2"><i class="fas fa-map-marker-alt "></i> Add Regions        <a class="float-right btn btn-small bg-lightblue" href="regions.php"><i class="fa fa-bars"></i> All Regions</a></h5>
+
+        <hr class="bg-lightblue">
+        </div>
+    </div>
+</div>
+
+  </section>
+<!-- start of scripts section -->
+  </div>
+<?php include("include/adminscript.php"); ?>
+<!-- end of scripts section -->
+
+</body>
+</html>
