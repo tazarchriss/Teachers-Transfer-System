a97af1791a4017a5803b4c392347084ae77004e8
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-05 23:42:29 +0300

    Create makatibu.php

---
+++ admin/makatibu.php
@@ -0,0 +1,43 @@
+<!-- This is the page for viewing all general secretaries -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TTS | Makatibu Wakuu</title>
+  <!-- start of admin css section -->
+  <?php include 'include/admincss.php'; ?>
+  <!-- end of admin css section -->
+
+</head>
+<!-- <style>
+    body{
+        background-image:url('bg.jpg');
+    }
+</style> -->
+<body class="hold-transition sidebar-mini layout-fixed bg-light">
+
+<!-- start of navbar section -->
+<?php include("include/navbar1.php"); ?>
+<!--  end of navbar section -->
+
+<div class="content-wrapper">
+<section class="content">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+        <h5 class="text-left  text-lightblue mx-2 mt-2"><i class="fa fa-user-tie"></i> Makatibu Wakuu         <a class="float-right btn btn-small bg-lightblue" href="ongeza_katibu.php"><i class="fa fa-user-plus"></i> Ongeza Katibu Mkuu</a></h5>
+
+        <hr class="bg-lightblue">
+        </div>
+    </div>
+</div>
+
+  </section>
+<!-- start of scripts section -->
+  </div>
+<?php include("include/adminscript.php"); ?>
+<!-- end of scripts section -->
+
+</body>
+</html>
